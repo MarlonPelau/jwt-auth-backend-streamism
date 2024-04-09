6619e50665b225174a802ff34088d7cc5f28437a
MarlonPelau <MarlonRegis@pursuit.org>
2024-04-09 11:55:48 -0400

    schema loaded and done for now

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,40 @@ CREATE DATABASE jwt_auth;
 
 \c jwt_auth
 
+DROP TABLE IF EXISTS streamers;
 
-CREATE TABLE users (
+CREATE TABLE streamers (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE,
+    username VARCHAR(200) UNIQUE NOT NULL,
+    password_hash VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE platforms (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  image TEXT NOT NULL,
+  rated INTEGER NOT NULL,
+  description VARCHAR(500),
+  mo TEXT,
+  link VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  content VARCHAR(500),
+  rating NUMERIC
+  CHECK(rating >= 1 AND rating <= 5),
+  created_at TEXT,
+  updated_at TEXT,
+  platform_id INTEGER REFERENCES platforms(id),
+  streamer_id INTEGER REFERENCES streamers(id)
+  ON DELETE CASCADE
+);
+
+
+
 
 
